6bd0cd13d88b4d6426a568229c15f0711491afaa
Mohamed <mhmdmrabet@yahoo.fr>
2022-01-12 14:05:02 +0100

    Add ratings icon

--- src/Components/MovieDetails/index.tsx
+++ src/Components/MovieDetails/index.tsx
@@ -5,6 +5,27 @@ type MovieDetailsProps = {
 };
 
 export const MovieDetails: React.FC<MovieDetailsProps> = ({ movie }) => {
+  // * Create array with 5 item and map into for create rating
+  function fillArrayWithNumbers(n: number) {
+    let arr = Array.apply(null, Array(5));
+    return arr.map(function (x, index) {
+      return (
+        <svg
+          key={index}
+          xmlns="http://www.w3.org/2000/svg"
+          className={`h-8 w-8 ${n > index ? 'text-sky-400' : ''}`}
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+        </svg>
+      );
+    });
+  }
+
+  // * Calculate rating
+  const rating = movie ? Math.round(movie?.vote_average / 2) : 0;
+
   // TODO Add ratings
   const MovieDetailsJSX = (
     <div className="p-8 space-y-4 text-2xl">
@@ -16,7 +37,7 @@ export const MovieDetails: React.FC<MovieDetailsProps> = ({ movie }) => {
         />
         <div className="space-y-4">
           <h2>{movie?.title}</h2>
-          <h3>{movie ? movie.vote_average / 2 : 0} / 5</h3>
+          <div className="flex">{fillArrayWithNumbers(rating)} / 5</div>
         </div>
       </div>
       <p className="text-lg">{movie?.overview}</p>
